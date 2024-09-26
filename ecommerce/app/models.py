5c68b16067956e05c151e696fa8099b11c8d4d16
Bruno Tres <btres76@gmail.com>
2024-09-26 10:19:51 -0300

    create str representation for models

--- ecommerce/app/models.py
+++ ecommerce/app/models.py
@@ -7,22 +7,30 @@ class User(models.Model):
     password = models.CharField(max_length=200)
     adress = models.CharField(max_length=200)
 
+    def __str__(self):
+        return f"ID {self.id} - {self.name}"
+
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=300)
     price = models.FloatField()
     stock = models.IntegerField()
+    def __str__(self):
+        return f"ID {self.id} - {self.name}"
+
+
+class Cart(models.Model):
+    total_price = models.FloatField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cart')
+    def __str__(self):
+        return f"ID {self.id} - Carrinho de {self.user.name}"
+    
 
 class OrderItem(models.Model):
     product_amount = models.IntegerField()
     total_item_price = models.FloatField()
-    product_id = models.ForeignKey(Product, null=True, on_delete=models.CASCADE, related_name='products')
-class Cart(models.Model):
-    total_price = models.FloatField()
-    #user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='carts')
-    #order_item_id = models.ForeignKey(orderItem, on_delete=models.PROTECT, related_name='items')
-
-
-
-
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='items')
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
+    def __str__(self):
+        return f"ID {self.id} - {self.product_amount}x {self.product.name} - R${self.total_item_price} "
