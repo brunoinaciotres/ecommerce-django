0bdfbed19debc633484abcb19cb1f8b0de791de6
Bruno Tres <btres76@gmail.com>
2024-09-26 10:36:58 -0300

    Create ForeignKey for Cart in User model

--- ecommerce/app/models.py
+++ ecommerce/app/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
+class Cart(models.Model):
+    total_price = models.FloatField()
+    def __str__(self):
+        return f"ID {self.id} - Carrinho de {self.user.name}"
 
 class User(models.Model):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=200)
     password = models.CharField(max_length=200)
     adress = models.CharField(max_length=200)
-
+    cart = models.ForeignKey(Cart, on_delete=models.DO_NOTHING, related_name="user")
     def __str__(self):
         return f"ID {self.id} - {self.name}"
 
@@ -19,14 +23,7 @@ class Product(models.Model):
     def __str__(self):
         return f"ID {self.id} - {self.name}"
 
-
-class Cart(models.Model):
-    total_price = models.FloatField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cart')
-    def __str__(self):
-        return f"ID {self.id} - Carrinho de {self.user.name}"
     
-
 class OrderItem(models.Model):
     product_amount = models.IntegerField()
     total_item_price = models.FloatField()
@@ -34,3 +31,5 @@ class OrderItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
     def __str__(self):
         return f"ID {self.id} - {self.product_amount}x {self.product.name} - R${self.total_item_price} "
+    
+
