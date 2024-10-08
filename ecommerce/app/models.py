fafefec0162ca1accfeae2c1e5142dd37faf525d
BRUNO INACIO TRES <bruno.tres@GRANBERY.local>
2024-10-08 19:36:58 -0300

    create IMG field in Product

--- ecommerce/app/models.py
+++ ecommerce/app/models.py
@@ -1,5 +1,13 @@
 import datetime
+import uuid
+
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+def get_file_path(_instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+    return filename
 
 class Cart(models.Model):
     total_price = models.FloatField()
@@ -19,18 +27,19 @@ class User(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=300)
-    price = models.FloatField()
-    stock = models.IntegerField()
+    name = models.CharField(max_length=100, null=False)
+    description = models.CharField(max_length=300, null=False)
+    img = models.ImageField(upload_to=get_file_path, null=True, blank=True)
+    price = models.FloatField(null=False)
+    stock = models.IntegerField(null=False)
     def __str__(self):
         return f"ID {self.id} - {self.name}"
     
 
     
 class OrderItem(models.Model):
-    product_amount = models.IntegerField()
-    total_item_price = models.FloatField()
+    product_amount = models.IntegerField(null=False)
+    total_item_price = models.FloatField(null=False)
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order_items')    
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="order_items")
     def __str__(self):
@@ -38,16 +47,17 @@ class OrderItem(models.Model):
 
 
 class Order(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    total_price = models.FloatField()
+    created_at = mod
+    els.DateTimeField(auto_now_add=True, null=False)
+    total_price = models.FloatField(null=False)
     cart = models.OneToOneField(Cart, primary_key=True, on_delete=models.DO_NOTHING)
 
 class Payment(models.Model):
-    method = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=30)
-    price_in_cents = models.FloatField()
+    method = models.IntegerField(null=False)
+    created_at = models.DateTimeField(auto_now_add=True, null=False)
+    updated_at = models.DateTimeField(auto_now=True, null=False)
+    status = models.CharField(max_length=30, null=False)
+    price_in_cents = models.FloatField(null=False)
     order = models.OneToOneField(Order, primary_key=True, on_delete=models.DO_NOTHING)    
 
 
