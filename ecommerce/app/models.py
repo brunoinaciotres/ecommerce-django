f0edfa72169ebac0756745b95b45539dcd8742ef
Bruno Tres <btres76@gmail.com>
2024-10-08 15:00:21 -0300

    update OrderItem and Payment class in models

--- ecommerce/app/models.py
+++ ecommerce/app/models.py
@@ -1,12 +1,6 @@
 import datetime
 from django.db import models
-
-class Cart(models.Model):
-    total_price = models.FloatField()
-    
-    def __str__(self):
-        return f"ID {self.id}"
-    
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
@@ -16,28 +10,24 @@ class Product(models.Model):
     def __str__(self):
         return f"ID {self.id} - {self.name}"
     
-
-    
-class OrderItem(models.Model):
-    product_amount = models.IntegerField()
-    total_item_price = models.FloatField()
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order_items')    
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="order_items")
-    def __str__(self):
-        return f"Id {self.id} - {self.product.name} - R${self.total_item_price} "
-
-
 class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     total_price = models.FloatField()
-    cart = models.OneToOneField(Cart, primary_key=True, on_delete=models.DO_NOTHING)
+    
+class OrderItem(models.Model):
+    product_amount = models.IntegerField()
+    product_price = models.FloatField(null=True, blank=True)
+    status = models.CharField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order_items')  
+    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name="order-items")  
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="order-items", null=True, blank=True)
+    def __str__(self):
+        return f"Id {self.id} - {self.product.name} - x{self.total_amount} "
 
 class Payment(models.Model):
     method = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=30)
-    price_in_cents = models.FloatField()
+    total_price = models.FloatField()
     order = models.OneToOneField(Order, primary_key=True, on_delete=models.DO_NOTHING)    
 
 
